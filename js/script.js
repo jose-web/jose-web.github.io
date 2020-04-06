5debdedb3fc04f23ba6321093608429d2342ad38
jose <delossantosramosjose@gmail.com>
2020-04-06 22:07:41 +0200

    Mejorada la visión de los repositorios separándolos en grupos

--- js/script.js
+++ js/script.js
@@ -3,31 +3,76 @@ xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         let json = JSON.parse(this.responseText);
 
-        let repositorios = document.getElementById("repositorios");
+        let arrayRepos = [];
+
         for (let i = 0; i < json.length; i++) {
             if (json[i].name != "jose-web.github.io") {
-                let repositorio = document.createElement("div");
-                repositorio.className = "repositorio";
-
-                let parrafo = document.createElement("p");
-                parrafo.innerHTML += json[i].name;
-                repositorio.appendChild(parrafo);
-
-                let enlace = document.createElement("a");
-                enlace.href = json[i].html_url;
-                enlace.innerHTML = "GitHub";
-                enlace.target = "_blank";
-                repositorio.appendChild(enlace);
-
-                if (json[i].has_pages) {
-                    enlace = document.createElement("a");
-                    enlace.href = json[i].name;
-                    enlace.innerHTML = "Web";
-                    enlace.target = "_blank";
-                    repositorio.appendChild(enlace);
+                if (json[i].description != null) {
+                    let descripcion = json[i].description.split("|")
+                    let tags = descripcion[1].split("#")
+                    for (let o = 1; o < tags.length; o++) {
+                        let etiqueta = tags[o].trim();
+                        if (typeof arrayRepos[etiqueta] == "undefined")
+                            arrayRepos[etiqueta] = [];
+                        arrayRepos[etiqueta].push(json[i]);
+                    }
                 }
-                repositorios.appendChild(repositorio);
+                if (typeof arrayRepos[json[i].language] == "undefined")
+                    arrayRepos[json[i].language] = [];
+                arrayRepos[json[i].language].push(json[i]);
             }
+        }
+
+        let repositorios = document.getElementById("repositorios");
+
+        for (const etiqueta in arrayRepos) {
+
+            let grupo = document.createElement("div");
+            grupo.className = "grupo";
+
+            let tituloGrupo = document.createElement("div");
+            tituloGrupo.className = "tituloGrupo";
+            tituloGrupo.innerHTML += etiqueta;
+            grupo.appendChild(tituloGrupo)
+
+            let grupoRepositorios = document.createElement("div");
+            grupoRepositorios.className = "grupoRepositorios";
+            grupo.appendChild(grupoRepositorios)
+
+            for (let i = 0; i < arrayRepos[etiqueta].length; i++) {
+                let repo = arrayRepos[etiqueta][i];
+
+                let repositorio = crearNodo({
+                    clase: "repositorio",
+                    padre: grupoRepositorios
+                });
+
+                crearNodo({
+                    clase: "nombreRepositorio",
+                    contenido: cambiaGuionPorEspacios(repo.name),
+                    padre: repositorio
+                });
+
+                crearNodo({
+                    tipo: "a",
+                    clase: "GitHub",
+                    href: repo.html_url,
+                    contenido: "GitHub",
+                    padre: repositorio
+                });
+
+                if (repo.has_pages) {
+                    crearNodo({
+                        tipo: "a",
+                        clase: "web",
+                        href: repo.name,
+                        contenido: "web",
+                        padre: repositorio
+                    });
+                }
+            }
+
+            repositorios.appendChild(grupo);
         }
     }
 };
@@ -35,4 +80,42 @@ xhttp.onreadystatechange = function () {
 window.onload = function () {
     xhttp.open("GET", "https://api.github.com/users/jose-web/repos", true);
     xhttp.send();
+}
+
+function crearNodo(opciones) {
+    let nodo;
+
+    if (typeof opciones.tipo == "undefined")
+        nodo = document.createElement("div");
+    else
+        nodo = document.createElement(opciones.tipo);
+
+
+    if (typeof opciones.clase != "undefined")
+        nodo.className = opciones.clase;
+
+    if (typeof opciones.href != "undefined")
+        nodo.href = opciones.href;
+
+    if (opciones.tipo == "a")
+        nodo.target = "_blank";
+
+    if (typeof opciones.contenido != "undefined")
+        nodo.innerHTML = opciones.contenido;
+
+
+    if (typeof opciones.padre != "undefined")
+        opciones.padre.appendChild(nodo);
+
+
+    return nodo
+
+
+}
+
+function cambiaGuionPorEspacios(palabra) {
+    while (palabra.includes("-")) {
+        palabra = palabra.replace("-", " ")
+    }
+    return palabra
 }
