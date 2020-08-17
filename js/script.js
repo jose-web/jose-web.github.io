25aa95fe81652939769283385befca66badbcfe8
jose <delossantosramosjose@gmail.com>
2020-08-17 21:25:28 +0200

    Se han añadido los repositorios

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ window.onload = function () {
     });
 
     muestraEdad()
-
+    muestraRepositorios()
 }
 
 function muestraEdad() {
@@ -105,4 +105,37 @@ function muestraEdad() {
         edad--;
     }
     document.getElementById("edad").innerHTML = edad
+}
+
+function muestraRepositorios() {
+    fetch("https://api.github.com/users/jose-web/repos", {
+        method: 'GET',
+        headers: { "Accept": "application/vnd.github.mercy-preview+json" }
+
+    }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(res => {
+
+            for (let i = 0; i < res.length; i++) {
+                if (res[i].name != "jose-web.github.io") {
+                    let repositorio = "<div><p>"
+                    repositorio += res[i].name
+                    repositorio += '<a title="Ver en GitHub" href="' + res[i].html_url + '" target="_blank"><i class="fab fa-github"></i></a>'
+                    repositorio += res[i].homepage == "" ? "" : '<a title="Ver la web" href="' + res[i].homepage + '" target="_blank"><i class="fas fa-external-link-alt"></i></a>'
+                    repositorio += "</p>"
+                    let topics = res[i].topics
+                    if (topics.length != 0) {
+                        repositorio += "<p>"
+                        for (let o = 0; o < topics.length; o++) {
+                            repositorio += "<span>" + topics[o] + "</span>"
+                        }
+                        repositorio += "</p>"
+                        console.log(res[i].html_url)
+                    }
+
+                    repositorio += "</div>"
+                    this.document.getElementById("repositorios").innerHTML += repositorio
+                }
+            }
+        });
 }
