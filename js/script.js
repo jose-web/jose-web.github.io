97a517a89adabb09391ae99b1e601145e8e4fb39
jose <delossantosramosjose@gmail.com>
2020-10-11 23:05:58 +0200

    Añadido el carrusel de las recomendaciones y eliminación de las particulas en el fondo

--- js/script.js
+++ js/script.js
@@ -1,97 +1,6 @@
 window.onload = function () {
-    particlesJS("particles-js", {
-        "particles": {
-            "number": {
-                "value": 80,
-                "density":
-                {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "color": {
-                "value": "#ffffff"
-            },
-            "shape": {
-                "type": "circle"
-            },
-            "opacity": {
-                "value": 1,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 0.2,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": false,
-                "distance": 150,
-                "color": "#ffffff",
-                "opacity": 0.05524295060491032,
-                "width": 1
-            },
-            "move": {
-                "enable": true,
-                "speed": 2,
-                "direction": "none",
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false,
-                    "mode": "bubble"
-                }, "onclick": {
-                    "enable": false,
-                    "mode": "repulse"
-                },
-                "resize": true
-            }, "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                }, "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                }
-            }
-        },
-        "retina_detect": true
-    });
-
     muestraEdad()
+    muestraComentarios()
     muestraRepositorios()
 }
 
@@ -105,6 +14,18 @@ function muestraEdad() {
         edad--;
     }
     document.getElementById("edad").innerHTML = edad
+}
+
+function muestraComentarios() {
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        dots: '.dots',
+        draggable: true,
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        }
+    })
 }
 
 function muestraRepositorios() {
