68455d6b778ee8c292c781fba8dee4712bd9b79e
jose <delossantosramosjose@gmail.com>
2020-10-29 17:24:31 +0100

    Añadidos repositorios y algunos cambios en el script para mostrar los repositorios

--- js/script.js
+++ js/script.js
@@ -25,19 +25,17 @@ function muestraRepositorios() {
         .then(res => {
 
             for (let i = 0; i < res.length; i++) {
-                if (res[i].name != "jose-web.github.io") {
+                let repositoriosOcultos = ["jose-web.github.io", "jose-web"]
+                if (!repositoriosOcultos.includes(res[i].name)) {
                     let repositorio = "<div><p>"
                     repositorio += res[i].name
-                    repositorio += '<a title="Ver en GitHub" href="' + res[i].html_url + '" target="_blank"><i class="fab fa-github"></i></a>'
-                    repositorio += res[i].homepage == "" ? "" : '<a title="Ver la web" href="' + res[i].homepage + '" target="_blank"><i class="fas fa-external-link-alt"></i></a>'
-                    repositorio += "</p>"
+                    repositorio += ' (Ver en <a title="Ver en GitHub" href="' + res[i].html_url + '" target="_blank">Github</a>'
+                    repositorio += res[i].homepage == "" ? "" : ' o en la <a title="Ver la web" href="' + res[i].homepage + '" target="_blank">Web</a>'
+                    repositorio += ")</p>"
                     let topics = res[i].topics
-                    if (topics.length != 0) {
-                        for (let o = 0; o < topics.length; o++) {
+                    if (topics.length != 0)
+                        for (let o = 0; o < topics.length; o++)
                             repositorio += "<span>" + topics[o] + "</span>"
-                        }
-                        console.log(res[i].html_url)
-                    }
 
                     repositorio += "</div>"
                     this.document.getElementById("repositorios").innerHTML += repositorio
