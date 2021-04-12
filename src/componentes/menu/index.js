c77fb9cfdfaf1a70c72c0fb151fe64c126a42306
jose <delossantosramosjose@gmail.com>
2021-04-12 20:31:53 +0200

    Creado el menú y los estilos comunes

---
+++ src/componentes/menu/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './estilos.scss'
+import { Link } from "react-router-dom"
+
+export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            select: (pagina) => props.pagina === pagina ? "selected" : ""
+        };
+    }
+
+    render() {
+        return (
+            <div id="menuPrincipal" >
+                <Link id="miNombre" to="/" title="JOSE DE LOS SANTOS RAMOS">JOSE-WEB</Link>
+                <ul>
+                    <li className={this.state.select("inicio")}><Link to="/" ><i class="fas fa-home"></i> Inicio</Link></li>
+                    <li className={this.state.select("experiencia")} ><Link to="/experiencia" ><i class="fas fa-briefcase"></i> Experiencia</Link></li>
+                    <li className={this.state.select("educacion")} ><Link to="/educacion" >‍<i class="fas fa-graduation-cap"></i> Educación</Link></li>
+                    <li className={this.state.select("proyectos")} ><Link to="/proyectos" ><i class="fas fa-file-code"></i> Proyectos</Link></li>
+                    {/* 
+                    <li>Administración
+                        <ul>
+                            <li><Link to="/" key="inicio" >Productos</Link></li>
+                            <li><Link to="/admin/facturas" key="facturas" >Facturas</Link></li>
+                            <li><Link to="/admin/usuarios" key="usuarios" >Usuarios</Link></li>
+                        </ul>
+                    </li> 
+                    */}
+                </ul>
+                <div id="contacto">
+                    <a href="https://www.linkedin.com/in/jose-web" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+
+                    <a href="mailto:delossantosramosjose@gmail.com" title="delossantosramosjose@gmail.com"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+        )
+    }
+}
--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './estilos.scss'
+import Menu from '../../componentes/menu'
 
 export default class Inicio extends React.Component {
 
     render() {
         return (
+            <>
+            <Menu pagina="inicio"/>
             <div id="inicio" >
                 inicio
             </div>
+            </>
         )
     }
 
