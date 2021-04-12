58e6ab0827bbc77e7ab07eff7ea2cd2d1d04fdc5
jose <delossantosramosjose@gmail.com>
2021-04-12 22:11:14 +0200

    Añadida página de inicio

--- src/paginas/inicio/index.js
+++ src/paginas/inicio/index.js
@@ -1,16 +1,55 @@
 import React from 'react'
 import './estilos.scss'
 import Menu from '../../componentes/menu'
+import inicio from '../../json/inicio.json'
 
 export default class Inicio extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            edad: this.muestraEdad()
+        };
+    }
+
+    muestraEdad() {
+        let hoy = new Date();
+        let cumpleanos = new Date("1996/12/13");
+        let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+        let m = hoy.getMonth() - cumpleanos.getMonth();
+
+        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+            edad--;
+        }
+        return edad
+    }
 
     render() {
         return (
             <>
-            <Menu pagina="inicio"/>
-            <div id="inicio" >
-                inicio
-            </div>
+                <Menu pagina="inicio" />
+                <main id="inicio" >
+                    <div>
+                        <img src="https://www.gravatar.com/avatar/9d08418b5f4ec35334131f12339e5e6d.jpg?s=256" alt="Avatar" />
+                        <h1>
+                            Hola,
+                            me llamo Jose de los santos
+                            y soy programador web
+                        </h1>
+                    </div>
+                    <div className="presentacion">
+                        <p className="titulo">Personal</p>
+                        <p>{inicio.presentacion.replace("$edad", this.state.edad)}</p>
+                    </div>
+                    <div className="presentacion">
+                        <p className="titulo">Sobre mi</p>
+                        <p>{inicio.sobreMi}</p>
+                    </div>
+                    <div className="presentacion">
+                        <p className="titulo">Trabajo en equipo</p>
+                        <p>{inicio.equipo}</p>
+                    </div>
+                </main>
             </>
         )
     }
